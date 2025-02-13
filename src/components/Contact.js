ed27c7b9fdda706d157e08eddd8c7814fc855856
jallupratibha <jallupratibha@gmail.com>
2025-02-13 18:03:26 -0500

    Initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/Contact.css';
+
+const Contact = () => {
+  return (
+    <div id="contact" className="contact-section">
+      <h2 className="contact-title">CONTACT</h2>
+      <h3 className="contact-subtitle">Connect with me 😊</h3>
+      <p className="contact-description">
+        Let’s connect! I’m available for inquiries, collaborations, and new opportunities to bring ideas to life. ✨
+      </p>
+      <div className="contact-buttons-container">
+        <a href="mailto:JAP508@pitt.edu" className="contact-button">
+          <span>Email Me</span>
+          <i className="fas fa-envelope"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/pratibha-jallu-b749a5227/" target="_blank" rel="noreferrer" className="contact-button">
+          <span>LinkedIn</span>
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://github.com/jallupratibha" target="_blank" rel="noreferrer" className="contact-button">
+          <span>Github</span>
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.behance.net/pratibhajallu" target="_blank" rel="noreferrer" className="contact-button">
+          <span>Behance</span>
+          <i className="fab fa-behance"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Hero.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import BackgroundAnimation from './BackgroundAnimation';
+import '../styles/Hero.css';
+
+const Hero = () => (
+  <div id="home" className="hero-container">
+    
+    <BackgroundAnimation /> {/* Add animated background */}
+    <div className="hero-content">
+      <p className="hero-intro">
+        Hello! It's Me <br />
+        <span className="hero-name">
+          PRATIBHA JALLU <span className="wave">👋</span>
+        </span>
+
+
+      </p>
+      <p className="hero-description">
+        Your Go-To UI/UX Designer for Exceptional Digital Experiences. I craft user-centric designs that blend creativity and functionality, transforming ideas into stunning, seamless interfaces for apps, websites, and more.
+      </p>
+      <p className="hero-subtext">
+        UI/UX Design, Product Design, Interaction Design? Consider it handled. 
+        <span className="drop-target">🎯</span>
+      </p>
+
+      <div className="stars-container">
+        {/* Generate multiple stars */}
+        {[...Array(15)].map((_, i) => (
+          <div key={i} className="star"></div>
+        ))}
+      </div>
+
+
+
+      <p className="hero-call-to-action">
+        Simple, yet impactful! Let’s create something amazing together!
+      </p>
+      <div className="hero-links">
+        <h3>FIND ME HERE!</h3>
+        <div className="links-container">
+          <div className="social-icons">
+            <a href="https://github.com/jallupratibha" target="_blank" rel="noreferrer">
+              <img src="/assets/github-icon.png" alt="GitHub" />
+            </a>
+            <a href="https://www.linkedin.com/in/pratibha-jallu-b749a5227/" target="_blank" rel="noreferrer">
+              <img src="/assets/linkedin-icon.png" alt="LinkedIn" />
+            </a>
+            <a href="https://www.behance.net/pratibhajallu" target="_blank" rel="noreferrer">
+              <img src="/assets/behance-icon.png" alt="Behance" />
+            </a>
+            <a href="mailto:JAP508@pitt.edu">
+              <img src="/assets/mail-icon.png" alt="Email" />
+            </a>
+          </div>
+          <a
+            href="https://drive.google.com/file/d/1jZ0PHIZrg5ju-946GnwfTENyeWz_NAly/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+            className="resume-btn">
+            Resume
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="hero-image">
+      <img src="/assets/Pratibha.jpg" alt="Pratibha Jallu" />
+    </div>
+  </div>
+);
+
+export default Hero;
